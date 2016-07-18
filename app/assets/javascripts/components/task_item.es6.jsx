f525d413180f48d9caf6a4ccedd402dc6c3058de
ginpei <ginpei@gmail.com>
2016-07-18 14:13:49 -0700

    split files by constructor

--- app/assets/javascripts/components/task_item.es6.jsx
+++ app/assets/javascripts/components/task_item.es6.jsx
@@ -54,36 +54,3 @@ TaskItem.propTypes = {
   working: React.PropTypes.bool,
   task: React.PropTypes.instanceOf(Object),
 };
-
-class TaskList extends React.Component {
-	render() {
-		var className = `task-list ${this.props.className || ''}`;
-
-		var children = this.props.tasks.map((v)=>{
-			return (<TaskItem
-				children={v.children || []}
-				id={v.id}
-				key={v.id}
-				name={v.name}
-				progress={v.progress}
-				task={v.task}
-				working={v.working}
-			/>)
-		});
-
-		return (
-			<div className={className}>
-				{children}
-			</div>
-		);
-	}
-
-	get defaultProps() {
-		return {
-			tasks: [],
-		};
-	}
-}
-TaskList.propTypes = {
-  tasks: React.PropTypes.instanceOf(Array),
-};
