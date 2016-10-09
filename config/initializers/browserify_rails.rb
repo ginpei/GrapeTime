1780ef5912ac39c3a1e94ab4cbef9684c26cc2c1
ginpei <ginpei@gmail.com>
2016-10-08 23:06:52 -0700

    show source maps
    Firefox seems not to support? Really?

--- config/initializers/browserify_rails.rb
+++ config/initializers/browserify_rails.rb
@@ -1,1 +1,2 @@
 Rails.application.config.browserify_rails.commandline_options = '-t [ babelify --presets [ es2015 react ] ]'
+Rails.application.config.browserify_rails.source_map_environments << 'development'
