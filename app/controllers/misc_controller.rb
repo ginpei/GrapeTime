a23262c96b1790a61f39aa37a77d16aeddacf75b
ginpei <ginpei@gmail.com>
2016-10-13 09:27:12 -0700

    login

--- app/controllers/misc_controller.rb
+++ app/controllers/misc_controller.rb
@@ -1,4 +1,14 @@
 class MiscController < ApplicationController
   def home
   end
+
+  def login
+    session[:user_id] = 1
+    redirect_to root_path
+  end
+
+  def logout
+    session[:user_id] = nil
+    redirect_to root_path
+  end
 end
